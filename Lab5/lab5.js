b16d358038ce1a45ee43d73516719f5bf3a4afd0
Gonzalo-AGM <a01702956@itesm.mx>
2021-03-13 20:29:00 -0600

    Completar js

--- Lab5/lab5.js
+++ Lab5/lab5.js
@@ -2,8 +2,25 @@ console.log('Hola, mundo!');
 
 const precio = 22;
 
-console.info('El precio de los tacos es de $' + precio)
+console.info('El precio de los tacos es de $' + precio);
 
-{
-    let cantidad_tacos = 0;
+let cantidad_tacos = 4;
+
+console.log('Te voy a mandar ' + cantidad_tacos + ' tacos');
+
+let persona = "Lalo";
+
+let descuento = 0;
+
+if(cantidad_tacos > 6 && persona === "Lalo"){
+    descuento = 0.1*(cantidad_tacos*precio);
+    console.info('Se te hizo un descuento de $' + descuento)
 }
+
+const por_pagar= cantidad_tacos * precio - descuento;
+
+const clientes = [];
+
+clientes.push('Lalo');
+
+console.log(clientes);
