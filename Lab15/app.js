ed28a44971050d7a46c7548b8c6cd0f54c97264d
Gonzalo-AGM <a01702956@itesm.mx>
2021-04-11 20:25:44 -0500

    crear Lab15

---
+++ Lab15/app.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const app = express();
+
+const rutaUserValidation = require('./routes/userValidation');
+const rutaTienda = require('./routes/tienda');
+
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+//Enviar archivos estáticos en carpeta public
+app.use(express.static(path.join(__dirname, 'public')));
+
+let contentType = 'text/html';
+let filePath = "";
+
+//Middleware
+app.use((request, response, next) => {
+    contentType = 'text/html';
+    filePath = '';
+
+    filePath = path.join(__dirname,'..',request.url);
+
+    let extName = path.extname(request.url);
+
+    switch (extName) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+    }
+
+    next();
+});
+
+app.use('/login', rutaUserValidation);
+
+app.use('/tienda', rutaTienda);
+
+app.use('/Recursos', (request, response, next) => {
+    response.render('Recursos', {
+        titulo: "Lab15-Recursos-GAGM-DAW & BD",
+        act1: "",
+        act2: "",
+        act3: "",
+        act4: "active",
+    });
+    console.log("Recursos");
+    response.status(200);
+});
+
+app.get('/', (request, response, next) => {
+    //filePath = path.join(__dirname,'..','views','lab14.html');
+    //response.writeHead(200, {'Content-Type': contentType});
+    //response.sendFile(filePath);
+    response.render('lab15', {
+        titulo: "Lab15-GAGM-DAW & BD",
+        act1: "active",
+        act2: "",
+        act3: "",
+        act4: "",
+    });
+    console.log("en home");
+    response.status(200);
+});
+
+app.use((request, response, next) => {
+    response.render('Err404', {
+        titulo: "Lab15-404-GAGM-DAW & BD",
+        act1: "",
+        act2: "",
+        act3: "",
+        act4: "",
+    });
+    console.log("404");
+    response.status(404);
+});
+
+app.listen(3000, function(){
+    console.log("server is running in port 3000");
+  });
---
+++ Lab15/routes/userValidation.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const router = express.Router();
+
+const path = require('path');
+
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({extended: false}));
+
+const fs = require('fs');
+
+const usuarios = [
+    {nombreUsuario: 'Admin', contrasena: '1234'}, 
+    {nombreUsuario: 'User', contrasena: 'noLeasEsto'}];
+
+//Enviar archivos estáticos en carpeta public
+router.use(express.static(path.join(__dirname,'..', 'public')));
+
+router.get('/RegPssw', (request, response, next) => {
+    response.render('RegPssw', {
+        titulo: "Lab14-login-GAGM-DAW & BD",
+        act1: "",
+        act2: "active",
+        act3: "",
+        act4: "",
+    });
+    console.log("Reg Pssw");
+    response.status(200);
+});
+
+router.post('/RegPssw', (request, response, next) => {
+    let username = request.body.username;
+    let pass = request.body.password;
+    usuarios.push({nombreUsuario: username, contrasena: pass});
+    user = "nombreUsuario: '" + username + "', contrasena: '" + pass + "\n";
+    fs.writeFileSync('login.txt', user, {encoding: "utf8", flag: "a+"});
+    response.status(302);
+    response.redirect('/login');
+});
+
+router.get('/ValidaPssw', (request, response, next) => {
+    response.render('ValidaPssw', {
+        titulo: "Lab14-login-GAGM-DAW & BD",
+        act1: "",
+        act2: "active",
+        act3: "",
+        act4: "",
+    });
+    console.log("Valida Pssw");
+    response.status(200);    
+});
+
+router.post('/ValidaPssw', (request, response, next) => {
+    let username = request.body.username;
+    let pass = request.body.pass;
+    const data = fs.readFileSync('login.txt');
+    if(data.includes("nombreUsuario: '"+ username +"', contrasena: '"+ pass)){
+        console.log("Acceso concedido");
+        response.status(302);
+        //window.alert("Contraseña correcta");
+        response.redirect('/tienda');
+    }else{
+        console.log("nombreUsuario: '"+ username +"', contrasena: '"+ pass);
+        console.log("Acceso denegado");
+        response.status(302);
+        //window.alert("Contraseña incorrecta");
+        response.redirect('/login/ValidaPssw');
+    }
+    response.end();
+});
+
+router.get('/', (request, response, next) => {
+    response.render('login', {
+        titulo: "Lab14-login-GAGM-DAW & BD",
+        act1: "",
+        act2: "active",
+        act3: "",
+        act4: "",
+    });
+    console.log("login");    
+    console.log(usuarios);
+    response.status(200);
+});
+
+module.exports = router;
